6c8bee3efc1a3388ee33d41feeee85f8a829c157
caarlosdamian <dapecarlos@gmail.com>
2024-07-22 17:34:26 -0600

    feat(listItem): added onClick/onDoubleClick functionality

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,14 +1,35 @@
-import './ListItem.css'
+import { useNavigate } from 'react-router-dom';
+import { getPokemonDetails } from '../../services/getPokemonDetails';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks/hook';
+import { setData } from '../../redux/slices/detailsSlice';
+import './ListItem.css';
 
 interface Props {
   name: string;
 }
 
 export const ListItem = ({ name }: Props) => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const state = useAppSelector((state) => state.details);
+
+  const handleClick = async () => {
+    const response = await getPokemonDetails(name);
+    dispatch(setData(response));
+  };
+
+  const handleDoubleClick = () => {
+    navigate(`/${name}`);
+  };
+
   return (
-    <li className='listItem'>
-      <span className='listItem__name'>{name}</span>
-      <img className='listItem__pokeball' src="/pokeball.png" alt="pokeball" />
+    <li
+      className={`listItem ${state?.name === name ? 'active' : ''} `}
+      onClick={handleClick}
+      onDoubleClick={handleDoubleClick}
+    >
+      <span className="listItem__name">{name}</span>
+      <img className="listItem__pokeball" src="/pokeball.png" alt="pokeball" />
     </li>
   );
 };
